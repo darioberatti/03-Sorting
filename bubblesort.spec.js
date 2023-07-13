d2901bbc43f21132049c5de44404aaa09155b628
darioberatti <137564078+darioberatti@users.noreply.github.com>
2023-07-13 18:17:17 -0300

    Update bubblesort.spec.js

--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -15,12 +15,4 @@ it("Un Arreglo ordenado que debe mantenerse ordenado.", function () {
 it("Un Arreglo a la inversa ([3,2,1]) que termine ordenado", function () {
   expect(bubbleSort([9, 7, 6, 4, 2])).toEqual([2, 4, 6, 7, 9]);
 });
-/* describe('probando spies', function() {
-  it('Un arreglo de 5 elementos debe hacer exactamente 4 swaps', function () {
-    spyOn(chocolate, 'masticar').and.callThrough(); 
-    // callThrough le dice al spy que utilice el método original chocolate['masticar'], 
-    // mientras que `callFake` haría una copia.
-    chocolate.comerTodo();
-    expect(chocolate.masticar.calls.count()).toEqual(3);
-  });
-}); */
+
